4e8266a7b6a6ba30198bea3af80721b3bf99ad73
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-05-11 02:35:24 +0200

    shift

---
+++ app/webroot/import_mysql_database/2016/coaches.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS `coaches` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `statement` text,
+  `biography` text,
+  `facebook` varchar(255) DEFAULT NULL,
+  `linkedin` varchar(255) DEFAULT NULL,
+  `video_file` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `gender` tinyint(1) NOT NULL DEFAULT '0',
+  `remote_coaching` tinyint(1) NOT NULL DEFAULT '0',
+  `approved` tinyint(1) NOT NULL DEFAULT '0',
+  `weight` int(11) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
