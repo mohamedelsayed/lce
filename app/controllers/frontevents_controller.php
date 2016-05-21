d6494462e71161f4d651062948357289a0df15f4
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-05-21 21:14:24 +0200

    shift

--- app/controllers/frontevents_controller.php
+++ app/controllers/frontevents_controller.php
@@ -6,9 +6,26 @@
  */
 class FronteventsController  extends AppController {
 	var $name = 'Frontevents';
-	var $uses = null;	
+	var $uses = 'Nevent';
 	function events(){
 		$this->set('title_for_layout' , 'All Events');	
-		$this->set('selected','frontevents');	
+		$this->set('selected','frontevents');
+		$year = isset($_GET['year'])?$_GET['year']:date("Y");
+		$month = isset($_GET['month'])?$_GET['month']:date("m");
+		$events = $this->Nevent->find('all', array(
+            'conditions' => array(
+                'AND' => array(
+                    'OR' => array(
+                        array('AND' => array(
+                                'YEAR(Nevent.start_date) = '.$year,
+                                'MONTH(Nevent.start_date) = '.$month
+                            )
+                        ),                          
+                    )
+                ),                             
+            ),
+            'order' => array('Nevent.start_date'=>'ASC','Nevent.id'=>'DESC'),
+        ));	
+        $this->set('events' , $events);
 	}
 }
