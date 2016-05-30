9110687a1c8f8183229194e14566cb30f02e64f8
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-05-31 00:49:08 +0200

    shift

--- app/views/layouts/front/events_instructors.php
+++ app/views/layouts/front/events_instructors.php
@@ -1,23 +1,61 @@
 <div id="mesagepopboxinstructorpopoup" class="mesage-pop" >
-    <div id="mesagecontent"></div>
+    <div class="mesagecontent"></div>
     <div class="mesage-pop-bg"></div>
 </div>
 <div id="mesagepopboxeventpopoup" class="mesage-pop" >
-    <div id="mesagecontent"></div>
+    <div class="mesagecontent"></div>
+    <div class="mesage-pop-bg"></div>
+</div>
+<div id="mesagepopboxcheckoutpopoup" class="mesage-pop" >
+    <div class="mesagecontent">
+    	<h4>
+            <span class="checkout_popup_title">Checkout</span>
+            <div class="closepopoup">X</div>
+        </h4>
+        <div class="checkout_popuppopoupbody">
+        	<form id="checkout_popup_form" action="<?php echo $base_url.'/vpc_php_serverhost_do';?>" method="post" accept-charset="UTF-8" enctype="multipart/form-data" onsubmit="return validate_checkout_form(jQuery(this));">
+	            <div class="form_item">
+	                <input id="popup_form_name" class="form_text" type="text" name="name" placeholder="Name" />    
+	            </div>
+	            <div class="form_item">
+	                <input id="popup_form_email" class="form_text" type="text" name="email" placeholder="Email" />    
+	            </div>
+	            <div class="form_item">
+	                <input id="popup_form_mobile_number" class="form_text" type="text" name="mobile_number" placeholder="Mobile Number" />    
+	            </div>
+	            <div>
+	            	<input type="submit" class="form_checkout_submit" value="Checkout" />
+	            </div>
+	            <input type="hidden" id="checkout_event_id" name="event_id" value="0">
+	        </form>
+	    </div>	    	
+    </div>
     <div class="mesage-pop-bg"></div>
 </div>
 <script type="text/javascript">
 function open_instructor_popup(content){
-    jQuery("#mesagepopboxinstructorpopoup #mesagecontent").html(content); 
-    jQuery("#mesagepopboxinstructorpopoup").addClass("alert"); 
-    jQuery("#mesagepopboxinstructorpopoup").show();
-    jQuery('body').addClass("mobile-menu-opend");       
+	jQuery("#mesagepopboxinstructorpopoup .mesagecontent").html(content);      
+    jQuery("#mesagepopboxinstructorpopoup").show();     
 }
 function close_instructor_popup(){
-    jQuery("#mesagepopboxinstructorpopoup").hide(); 
-    jQuery("#mesagepopboxinstructorpopoup #mesagecontent").html('');
-    jQuery("#mesagepopboxinstructorpopoup").removeClass("alert");
-    jQuery('body').removeClass("mobile-menu-opend");          
+	jQuery("#mesagepopboxinstructorpopoup").hide(); 
+    jQuery("#mesagepopboxinstructorpopoup .mesagecontent").html('');      
+}
+function open_event_popup(content){
+	jQuery("#mesagepopboxeventpopoup .mesagecontent").html(content);     
+    jQuery("#mesagepopboxeventpopoup").show();
+}
+function close_event_popup(){
+	jQuery("#mesagepopboxeventpopoup").hide(); 
+    jQuery("#mesagepopboxeventpopoup .mesagecontent").html('');         
+}
+function open_checkout_event_form_popup(id){  
+	jQuery('#checkout_event_id').val(id);
+	jQuery("#mesagepopboxcheckoutpopoup").show();	
+}
+function close_checkout_event_form_popup(){
+	jQuery("#mesagepopboxcheckoutpopoup").hide();        
+	jQuery('#checkout_event_id').val(0);
 }
 function open_instructor(id){
     jQuery.ajax({
@@ -31,18 +69,6 @@ function open_instructor(id){
         }
     }); 
 }
-function open_event_popup(content){
-    jQuery("#mesagepopboxeventpopoup #mesagecontent").html(content); 
-    jQuery("#mesagepopboxeventpopoup").addClass("alert"); 
-    jQuery("#mesagepopboxeventpopoup").show();
-    jQuery('body').addClass("mobile-menu-opend");       
-}
-function close_event_popup(){
-    jQuery("#mesagepopboxeventpopoup").hide(); 
-    jQuery("#mesagepopboxeventpopoup #mesagecontent").html('');
-    jQuery("#mesagepopboxeventpopoup").removeClass("alert");
-    jQuery('body').removeClass("mobile-menu-opend");          
-}
 function open_event(id){
     jQuery.ajax({
     	url: base_url+'/frontevents/get_event/'+id,
@@ -55,10 +81,80 @@ function open_event(id){
         }
     }); 
 }
+function checkout_event(id){  
+	close_event_popup();
+	open_checkout_event_form_popup(id);	
+}
+function validate_checkout_form(obj) {		
+	var form_id = obj.attr('id');
+	validate_required_input(jQuery('#popup_form_name'));
+	validate_email_input(jQuery('#popup_form_email'));	
+	validate_numeric_input(jQuery('#popup_form_mobile_number'));
+    var checkout_form_flag = 0;
+	var focused = 0;
+	jQuery('#'+form_id+' input').each(function(){
+		if(jQuery(this).hasClass('error')){ 
+			checkout_form_flag = 1;
+			if(focused == 0){
+	    		focused = 1;
+		    	jQuery(this).focus();
+		    }
+		}
+	});
+	if(checkout_form_flag === 0){
+		return true;
+	}else{
+		return false;
+	}   	
+}
+function validate_required_input(obj){
+	var val = obj.val();
+	if (jQuery.trim(val).length !== 0){
+		if(obj.hasClass('error')){
+			obj.removeClass("error");			
+		}
+	}else{
+		if(!(obj.hasClass('error'))){
+			obj.addClass("error");			
+		}			
+	}
+}
+function validate_email_input(obj){
+	var val = obj.val();
+	if (isValidEmailAddress(val)){
+		if(obj.hasClass('error')){
+			obj.removeClass("error");			
+		}
+	}else{
+		if(!(obj.hasClass('error'))){
+			obj.addClass("error");			
+		}			
+	}
+}
+function validate_numeric_input(obj){
+	var val = obj.val();
+	if (isNumeric(val)){
+		if(obj.hasClass('error')){
+			obj.removeClass("error");			
+		}
+	}else{
+		if(!(obj.hasClass('error'))){
+			obj.addClass("error");			
+		}			
+	}
+}
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+};
+function isNumeric(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
 jQuery(document).ready(function() {
     jQuery("body").on("click",".closepopoup, .mesage-pop-bg", function(){
     	close_instructor_popup(); 
-    	close_event_popup();            
+    	close_event_popup();          
+    	close_checkout_event_form_popup();  
     });
 });
 </script>
