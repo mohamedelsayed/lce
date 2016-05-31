e5b392e21ad7ac3276c760579f3117cb30877b36
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-05-31 02:05:15 +0200

    shift

--- app/controllers/frontevents_controller.php
+++ app/controllers/frontevents_controller.php
@@ -136,16 +136,16 @@ class FronteventsController  extends AppController {
                 )           
             );
             $model = 'Nevent';
-			$model2 = 'Instructor'; 
 			if(isset($event[$model]['ticket_price'])){
 				$event_amount = $event[$model]['ticket_price'];
+				$title = $event[$model]['title'];
 			}
 			if($event_amount > 0 && is_numeric($event_amount)){
-				$_POST["Title"] = 'LCE Event Order';
+				$_POST["Title"] = $title;
 				$_POST["virtualPaymentClientURL"] = 'https://migs.mastercard.com.au/vpcpay';
 				$_POST["vpc_Version"] = '1';
 				$_POST["vpc_Command"] = 'pay';
-				$_POST["vpc_OrderInfo"] = 'LCE Event Order';					
+				$_POST["vpc_OrderInfo"] = 'LCE Event Order - '.$title;					
 				$vpcURL = $_POST["virtualPaymentClientURL"] . "?";
 				unset($_POST["virtualPaymentClientURL"]); 
 				unset($_POST["SubButL"]);
@@ -190,8 +190,211 @@ class FronteventsController  extends AppController {
 		}
 	}
 	function return_transaction(){
-		$this->set('title_for_layout' , 'Successful Transaction');	
+		$custom_error_flag = 1;
+		$custom_message = 'Error';
+		$amount = 0;			
+		$title = '';
+		$location = '';
+		$ticket_price = '';
+		$instructor_id = 0;
+		$instructor_name = '';
+		$all_date = '';
 		$this->set('selected', 'frontevents');
-		$SECURE_SECRET = $this->payment_hash_secret;				
+		$SECURE_SECRET = $this->payment_hash_secret;
+		$vpc_Txn_Secure_Hash = $_GET["vpc_SecureHash"];
+		unset($_GET["vpc_SecureHash"]); 				
+		// set a flag to indicate if hash has been validated
+		$errorExists = false;		
+		if (strlen($SECURE_SECRET) > 0 && $_GET["vpc_TxnResponseCode"] != "7" && $_GET["vpc_TxnResponseCode"] != "No Value Returned") {		
+		    $md5HashData = $SECURE_SECRET;		
+		    // sort all the incoming vpc response fields and leave out any with no value
+		    foreach($_GET as $key => $value) {
+		        if ($key != "vpc_SecureHash" or strlen($value) > 0) {
+		            $md5HashData .= $value;
+		        }
+		    }		    
+		    // Validate the Secure Hash (remember MD5 hashes are not case sensitive)
+			// This is just one way of displaying the result of checking the hash.
+			// In production, you would work out your own way of presenting the result.
+			// The hash check is all about detecting if the data has changed in transit.
+		    if (strtoupper($vpc_Txn_Secure_Hash) == strtoupper(md5($md5HashData))) {
+		        // Secure Hash validation succeeded, add a data field to be displayed
+		        // later.
+		        $hashValidated = "<FONT color='#00AA00'><strong>CORRECT</strong></FONT>";
+		    } else {
+		        // Secure Hash validation failed, add a data field to be displayed
+		        // later.
+		        $hashValidated = "<FONT color='#FF0066'><strong>INVALID HASH</strong></FONT>";
+		        $errorExists = true;
+		    }
+		} else {
+		    // Secure Hash was not validated, add a data field to be displayed later.
+		    $hashValidated = "<FONT color='orange'><strong>Not Calculated - No 'SECURE_SECRET' present.</strong></FONT>";
+		}		
+		// Define Variables
+		// ----------------
+		// Extract the available receipt fields from the VPC Response
+		// If not present then let the value be equal to 'No Value Returned'		
+		// Standard Receipt Data
+		$amount          = $this->null2unknown($_GET["vpc_Amount"]);
+		$locale          = $this->null2unknown($_GET["vpc_Locale"]);
+		$batchNo         = $this->null2unknown($_GET["vpc_BatchNo"]);
+		$command         = $this->null2unknown($_GET["vpc_Command"]);
+		$message         = $this->null2unknown($_GET["vpc_Message"]);
+		$version         = $this->null2unknown($_GET["vpc_Version"]);
+		$cardType        = $this->null2unknown($_GET["vpc_Card"]);
+		$orderInfo       = $this->null2unknown($_GET["vpc_OrderInfo"]);
+		$receiptNo       = $this->null2unknown($_GET["vpc_ReceiptNo"]);
+		$merchantID      = $this->null2unknown($_GET["vpc_Merchant"]);
+		$authorizeID     = $this->null2unknown($_GET["vpc_AuthorizeId"]);
+		$merchTxnRef     = $this->null2unknown($_GET["vpc_MerchTxnRef"]);
+		$transactionNo   = $this->null2unknown($_GET["vpc_TransactionNo"]);
+		$acqResponseCode = $this->null2unknown($_GET["vpc_AcqResponseCode"]);
+		$txnResponseCode = $this->null2unknown($_GET["vpc_TxnResponseCode"]);
+		// 3-D Secure Data
+		$verType         = array_key_exists("vpc_VerType", $_GET)          ? $_GET["vpc_VerType"]          : "No Value Returned";
+		$verStatus       = array_key_exists("vpc_VerStatus", $_GET)        ? $_GET["vpc_VerStatus"]        : "No Value Returned";
+		$token           = array_key_exists("vpc_VerToken", $_GET)         ? $_GET["vpc_VerToken"]         : "No Value Returned";
+		$verSecurLevel   = array_key_exists("vpc_VerSecurityLevel", $_GET) ? $_GET["vpc_VerSecurityLevel"] : "No Value Returned";
+		$enrolled        = array_key_exists("vpc_3DSenrolled", $_GET)      ? $_GET["vpc_3DSenrolled"]      : "No Value Returned";
+		$xid             = array_key_exists("vpc_3DSXID", $_GET)           ? $_GET["vpc_3DSXID"]           : "No Value Returned";
+		$acqECI          = array_key_exists("vpc_3DSECI", $_GET)           ? $_GET["vpc_3DSECI"]           : "No Value Returned";
+		$authStatus      = array_key_exists("vpc_3DSstatus", $_GET)        ? $_GET["vpc_3DSstatus"]        : "No Value Returned";		
+		// *******************
+		// END OF MAIN PROGRAM
+		// *******************
+		
+		// FINISH TRANSACTION - Process the VPC Response Data
+		// =====================================================
+		// For the purposes of demonstration, we simply display the Result fields on a
+		// web page.
+		
+		// Show 'Error' in title if an error condition
+		$errorTxt = "";		
+		// Show this page as an error page if vpc_TxnResponseCode equals '7'
+		if ($txnResponseCode == "7" || $txnResponseCode == "No Value Returned" || $errorExists) {
+		    $errorTxt = "Error ";
+		}		    
+		// This is the display title for 'Receipt' page 
+		//$title = $_GET["Title"];
+		$event_id = 0;
+		if(isset($_GET['event_id'])){
+			$event_id = $_GET['event_id'];			
+		}		
+		if($event_id > 0 && is_numeric($event_id)){
+			$event = $this->Nevent->find(
+                'first', array(
+                    'conditions' => array('Nevent.approved' => 1, 'Nevent.id' => $event_id),
+                )           
+            );
+            $model = 'Nevent';	
+			$model2 = 'Instructor'; 	
+			if(!empty($event)){	
+				$title = $event[$model]['title'];
+				$location = $event[$model]['location'];
+				$ticket_price = $event[$model]['ticket_price'];
+				$instructor_id = $event[$model]['instructor_id'];
+				$instructor_name = $event[$model2]['name'];
+				$date = date('F d, Y', strtotime($event[$model]['start_date']));
+				$time_from = date('g:i a', strtotime($event[$model]['time_from']));
+				$time_to = date('g:i a', strtotime($event[$model]['time_to']));
+				$duration = $event[$model]['duration'];		
+				$all_date = $date;
+				if($duration > 1){
+					$all_date .= ' '.$duration.' Days';
+				}
+				$all_date .= ' <br />'.$time_from.' to '.$time_to;          
+			} 
+			if($txnResponseCode == 0){
+				$custom_error_flag = 0;
+				$custom_message = 'successful transaction';
+			}else{
+				$custom_error_flag = 1;
+				$custom_message = $this->getResponseDescription($txnResponseCode);
+			}
+			$name = '';
+			if(isset($_GET['name'])){
+				$name = $_GET['name'];
+			}
+			$email = '';
+			if(isset($_GET['email'])){
+				$email = $_GET['email'];
+			}
+			$mobile_number = '';
+			if(isset($_GET['mobile_number'])){
+				$mobile_number = $_GET['mobile_number'];
+			}
+			$transactionNo;
+			$receiptNo;
+			$amount;
+		}
+		$this->set('custom_message' , $custom_message);
+		$this->set('custom_error_flag' , $custom_error_flag);
+		$this->set('amount' , $amount);
+		$this->set('title' , $title);
+		$this->set('location' , $location);
+		$this->set('ticket_price' , $ticket_price);
+		$this->set('instructor_id' , $instructor_id);
+		$this->set('instructor_name' , $instructor_name);
+		$this->set('all_date' , $all_date);		
+		$this->set('title_for_layout' , $custom_message);		
 	}
+	function null2unknown($data) {
+	    if ($data == "") {
+	        return "No Value Returned";
+	    } else {
+	        return $data;
+	    }
+	} 
+	function getResponseDescription($responseCode) {
+	    switch ($responseCode) {
+	        case "0" : $result = "Transaction Successful"; break;
+	        case "?" : $result = "Transaction status is unknown"; break;
+	        case "1" : $result = "Unknown Error"; break;
+	        case "2" : $result = "Bank Declined Transaction"; break;
+	        case "3" : $result = "No Reply from Bank"; break;
+	        case "4" : $result = "Expired Card"; break;
+	        case "5" : $result = "Insufficient funds"; break;
+	        case "6" : $result = "Error Communicating with Bank"; break;
+	        case "7" : $result = "Payment Server System Error"; break;
+	        case "8" : $result = "Transaction Type Not Supported"; break;
+	        case "9" : $result = "Bank declined transaction (Do not contact Bank)"; break;
+	        case "A" : $result = "Transaction Aborted"; break;
+	        case "C" : $result = "Transaction Cancelled"; break;
+	        case "D" : $result = "Deferred transaction has been received and is awaiting processing"; break;
+	        case "F" : $result = "3D Secure Authentication failed"; break;
+	        case "I" : $result = "Card Security Code verification failed"; break;
+	        case "L" : $result = "Shopping Transaction Locked (Please try the transaction again later)"; break;
+	        case "N" : $result = "Cardholder is not enrolled in Authentication scheme"; break;
+	        case "P" : $result = "Transaction has been received by the Payment Adaptor and is being processed"; break;
+	        case "R" : $result = "Transaction was not processed - Reached limit of retry attempts allowed"; break;
+	        case "S" : $result = "Duplicate SessionID (OrderInfo)"; break;
+	        case "T" : $result = "Address Verification Failed"; break;
+	        case "U" : $result = "Card Security Code Failed"; break;
+	        case "V" : $result = "Address Verification and Card Security Code Failed"; break;
+	        default  : $result = "Unable to be determined"; 
+	    }
+	    return $result;
+	}
+	function getStatusDescription($statusResponse) {
+	    if ($statusResponse == "" || $statusResponse == "No Value Returned") {
+	        $result = "3DS not supported or there was no 3DS data provided";
+	    } else {
+	        switch ($statusResponse) {
+	            Case "Y"  : $result = "The cardholder was successfully authenticated."; break;
+	            Case "E"  : $result = "The cardholder is not enrolled."; break;
+	            Case "N"  : $result = "The cardholder was not verified."; break;
+	            Case "U"  : $result = "The cardholder's Issuer was unable to authenticate due to some system error at the Issuer."; break;
+	            Case "F"  : $result = "There was an error in the format of the request from the merchant."; break;
+	            Case "A"  : $result = "Authentication of your Merchant ID and Password to the ACS Directory Failed."; break;
+	            Case "D"  : $result = "Error communicating with the Directory Server."; break;
+	            Case "C"  : $result = "The card type is not supported for authentication."; break;
+	            Case "S"  : $result = "The signature on the response received from the Issuer could not be validated."; break;
+	            Case "P"  : $result = "Error parsing input from Issuer."; break;
+	            Case "I"  : $result = "Internal Payment Server system error."; break;
+	            default   : $result = "Unable to be determined"; break;
+	        }
+	    }
+	    return $result;
+	}	
 }
