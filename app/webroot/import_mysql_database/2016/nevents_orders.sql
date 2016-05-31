9334a35ccbb8751f5da29c090b9b8d1b0e51f8b5
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-05-31 05:35:15 +0200

    shift

---
+++ app/webroot/import_mysql_database/2016/nevents_orders.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE IF NOT EXISTS `nevents_orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile_number` varchar(255) NOT NULL,
+  `receipt_number` varchar(255) NOT NULL,
+  `transaction_number` varchar(255) NOT NULL,
+  `event_id` int(11) NOT NULL DEFAULT '0',
+  `amount` int(11) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
