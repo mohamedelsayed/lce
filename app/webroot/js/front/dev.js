a441d8354487b1f8b04cc21bb3f7570e9411ff65
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-06-04 23:48:36 +0200

    shift

--- app/webroot/js/front/dev.js
+++ app/webroot/js/front/dev.js
@@ -251,6 +251,7 @@ function validate_contactme_form(obj) {
         success: function(result) {
         	var html = result.html;
         	jQuery(".form_contactme_submit").removeAttr("disabled");
+        	close_contact_me_popup();
         	open_coach_popup(html);
         }
     }); 
