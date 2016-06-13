6b908f045074088bacfcc1787f83a46f2984561c
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-06-13 23:34:48 +0200

    shift

--- app/elsayed_db.php
+++ app/elsayed_db.php
@@ -3,7 +3,7 @@
  * @author Author "Mohamed Elsayed"  
  * @author Author Email "me@mohamedelsayed.net"
  * @link http://www.lifecoachingegypt.net
- * @copyright Copyright (c) 2015 Programming by "http://www.mohamedelsayed.net"
+ * @copyright Copyright (c) 2016 Programming by "http://www.mohamedelsayed.net"
  */
 $http_host = $_SERVER['HTTP_HOST'];
 $db_host = 'localhost';
@@ -18,10 +18,12 @@ $base_url = $http_string . $http_host;
 if (strpos($http_host, '.mohamedelsayed.net') !== FALSE) {
 	$database = 'elsayed_lce';
 	$username = 'elsayed_lce';
-    $password = 'xPBNZvrd8u8z';	
+    $password = 'xPBNZvrd8u8z';
 }
 if (strpos($http_host, 'lifecoachingegypt.com') !== FALSE) {
-    $database = 'lce';
+    $database = 'lifecoj0_lifecoa_chingwebsite';
+	$username = 'lifecoj0_chidbus';
+    $password = 'L0gmeuIn0W3';
 }elseif (strpos($http_host, 'localhost') !== FALSE) {
 	$base_url = $http_string . $http_host . '/myworkspace/lce';
     if (PHP_OS == 'Linux') {
