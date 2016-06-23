dbee30f828982abdbf4f7f261680e94e0cf76d28
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-06-23 05:17:05 +0200

    shift

--- app/webroot/import_mysql_database/2016/nevents_orders.sql
+++ app/webroot/import_mysql_database/2016/nevents_orders.sql
@@ -13,3 +13,6 @@ CREATE TABLE IF NOT EXISTS `nevents_orders` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 ALTER TABLE `nevents_orders` ADD `tickets_number` INT NOT NULL DEFAULT '1';
+
+INSERT INTO `contents` (`id`, `title`, `body`, `map_iframe`, `address`, `phone`, `mail`, `facebook_link`, `linkedin_link`, `working_hours`, `inner_title`) VALUES
+(2, 'Terms and Conditions', '<p>\r\n  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\r\n', '', '', '', '', '', '', '', '');
