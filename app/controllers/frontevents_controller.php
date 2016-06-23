9106aa3421970943449b7168f48554eba7941aff
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-06-23 15:01:52 +0200

    shift

--- app/controllers/frontevents_controller.php
+++ app/controllers/frontevents_controller.php
@@ -182,7 +182,7 @@ class FronteventsController  extends AppController {
 				$_POST["virtualPaymentClientURL"] = 'https://migs.mastercard.com.au/vpcpay';
 				$_POST["vpc_Version"] = '1';
 				$_POST["vpc_Command"] = 'pay';
-				$_POST["vpc_OrderInfo"] = $event_id.'-event'.time();					
+				$_POST["vpc_OrderInfo"] = $event_id.'-'.Inflector::slug(strtolower($title), '-').time();					
 				$vpcURL = $_POST["virtualPaymentClientURL"] . "?";
 				unset($_POST["virtualPaymentClientURL"]); 
 				unset($_POST["SubButL"]);
