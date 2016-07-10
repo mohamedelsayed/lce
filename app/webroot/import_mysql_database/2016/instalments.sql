45082eab799a26636fa7de434414042c56c8d606
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-07-10 07:00:10 +0200

    shift

--- app/webroot/import_mysql_database/2016/instalments.sql
+++ app/webroot/import_mysql_database/2016/instalments.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `settings` ADD `number_of_instalments` INT NOT NULL DEFAULT '1',
 ADD `value_for_each_installment` INT NOT NULL DEFAULT '1';
+ALTER TABLE `nevents_orders` ADD `installment_flag` INT NOT NULL DEFAULT '0';
