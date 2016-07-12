1c639e8984961b01702cda37f7bf6d71ac565b21
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-07-12 22:51:09 +0200

    shift

--- app/webroot/import_mysql_database/2016/settings.sql
+++ app/webroot/import_mysql_database/2016/settings.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `settings` ADD `hide_geography` BOOLEAN NOT NULL DEFAULT '0',
 ADD `coaches_email` VARCHAR( 255 ) NULL ;
+ALTER TABLE `settings` ADD `payment_email` TEXT NOT NULL;
+ALTER TABLE `settings` CHANGE `coaches_email` `coaches_email` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
