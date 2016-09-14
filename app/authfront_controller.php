9088133f5719cf1a786530ae7c3ef4b09c14d79c
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-09-15 01:48:28 +0200

    shift

--- app/authfront_controller.php
+++ app/authfront_controller.php
@@ -62,7 +62,7 @@ class AuthfrontController extends AppController{
 		$this->set('isAdmin', $isAdmin);	
 	}
 	function beforeRender(){
-		//$this->setParentCat();
+		$this->setParentCat();
 		//$this->setHeaderQuotes();
 		if($this->layout != 'ajax'){
 			$this->layout = 'forum/main';
