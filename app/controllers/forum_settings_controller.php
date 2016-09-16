add5e371374a72f86adb96e50415e1be28aaa50f
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-09-16 23:25:55 +0200

    shift

---
+++ app/controllers/forum_settings_controller.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Author "Mohamed Elsayed"  
+ * @author Author Email "me@mohamedelsayed.net"
+ * @copyright Copyright (c) 2016 Programming by "mohamedelsayed.net"
+ */
+require_once '../authfront_controller.php';
+class ForumSettingsController extends AuthfrontController {
+	var $name = 'ForumSettings';
+	var $uses = array('Setting');		
+	function index() {
+		$this->set('selected','adminpages');
+		if (!empty($this->data)) {
+			if ($this->Setting->save($this->data)){
+				//reset session settings
+				//$this->setSettings(); 
+				$this->Session->setFlash(__('The Settings has been saved', true));
+				$this->redirect(array('controller' => 'ForumSettings', 'action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The Settings could not be saved. Please, try again.', true));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->Setting->read(null, 2);
+		}
+	}
+}
