c96d96657f802a0bd8c6f50585d93dc7edd7845a
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-09-24 20:59:40 +0200

    shift

--- app/webroot/js/forum/dev.js
+++ app/webroot/js/forum/dev.js
@@ -1,3 +1,34 @@
+/**
+ * @author Author "Mohamed Elsayed"  
+ * @author Author Email "me@mohamedelsayed.net"
+ * @link http://www.lifecoachingegypt.com
+ * @copyright Copyright (c) 2016 Programming by "mohamedelsayed.net"
+ */
 function open_event(id){
 	window.location.href = base_url+"/events/view/"+id;	    
+}
+jQuery(document).ready(function(){
+	jQuery("body").on("click",".closepopoup, .mesage-pop-bg", function(){
+    	close_instructor_popup();     	
+    });
+});
+function open_instructor_popup(content){
+	jQuery("#mesagepopboxinstructorpopoup .mesagecontent").html(content);      
+    jQuery("#mesagepopboxinstructorpopoup").show();     
+}
+function close_instructor_popup(){
+	jQuery("#mesagepopboxinstructorpopoup").hide(); 
+    jQuery("#mesagepopboxinstructorpopoup .mesagecontent").html('');      
+}
+function open_instructor(id){
+    jQuery.ajax({
+    	url: base_url+'/frontevents/get_instructor/'+id,
+        type: 'GET',
+        beforeSend: function() {
+        	open_instructor_popup('<h4>Loading<div id="closeinstructorpopoup" class="closeinstructorpopoup closepopoup">X</div></h4><div class="instructor_loading ajax_loading"></div>');            
+        },
+        success: function(result) {
+        	open_instructor_popup(result);            
+        }
+    }); 
 }
