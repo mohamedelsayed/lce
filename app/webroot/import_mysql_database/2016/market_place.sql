e8f8a1a8698a850531505b6187cdd2b6d619d74d
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-09-28 00:04:44 +0200

    shift

--- app/webroot/import_mysql_database/2016/market_place.sql
+++ app/webroot/import_mysql_database/2016/market_place.sql
@@ -1,3 +1,4 @@
 TRUNCATE categories;
 TRUNCATE posts;
 ALTER TABLE `attachments` ADD `post_id` INT NOT NULL DEFAULT '0';
+ALTER TABLE `attachments` CHANGE `title` `title` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
