158cad2e366708edb58d036b4a5a59a77e0a0e9a
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-10-02 01:01:30 +0200

    shift

---
+++ app/models/library.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Author "Mohamed Elsayed"  
+ * @author Author Email "me@mohamedelsayed.net"
+ * @copyright Copyright (c) 2016 Programming by "mohamedelsayed.net"
+ */
+class Library extends AppModel {
+	var $name = 'Library';
+	var $displayField = 'title';
+	var $validate = array(
+		'title' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				'message' => 'Title cannot be left blank',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		)
+	);
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+	var $belongsTo = array(	
+	);
+	var $hasMany = array(
+	);
+}
