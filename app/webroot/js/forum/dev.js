914446b93421ae5874735571ce99fc96ae362250
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-10-09 03:36:54 +0200

    shift

--- app/webroot/js/forum/dev.js
+++ app/webroot/js/forum/dev.js
@@ -14,10 +14,13 @@ jQuery(document).ready(function(){
     jQuery("body").on("change","#LibraryType2", function(){  
     	var type = jQuery(this).val(); 
     	var youtube_url = jQuery('#LibraryYoutubeUrl');
+    	var google_drive_url = jQuery('#LibraryGoogleDriveUrl');
     	if(type == 5){    		
-    		youtube_url.parent().fadeIn(200);
+    		youtube_url.parent().show();
+    		google_drive_url.parent().hide();
     	}else{
-    		youtube_url.parent().fadeOut(200);    		
+    		youtube_url.parent().hide();
+    		google_drive_url.parent().show();    		
     	}
     });
     jQuery("body").on("click", ".removeuploadfilebtn", function () {
