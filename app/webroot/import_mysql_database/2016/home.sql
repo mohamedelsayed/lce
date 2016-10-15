aaedff2c7c062c8f47a1d2adc924a2800c56c8b7
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-10-15 07:31:25 +0200

    shift

--- app/webroot/import_mysql_database/2016/home.sql
+++ app/webroot/import_mysql_database/2016/home.sql
@@ -2,20 +2,6 @@ ALTER TABLE `slideshows` ADD `forum_flag` INT NOT NULL DEFAULT '0';
 ALTER TABLE `contents` ADD `image` VARCHAR( 255 ) NULL ,
 ADD `forum_flag` INT NOT NULL DEFAULT '0';
 ALTER TABLE `contents` DROP `image`;
-INSERT INTO `lce`.`contents` (
-`id` ,
-`title` ,
-`body` ,
-`map_iframe` ,
-`address` ,
-`phone` ,
-`mail` ,
-`facebook_link` ,
-`linkedin_link` ,
-`working_hours` ,
-`inner_title` ,
-`forum_flag`
-)
-VALUES (
-NULL , 'Welcome', '', NULL , NULL , NULL , NULL , NULL , NULL , NULL , NULL , '0'
-);
+INSERT INTO `contents` (`id`, `title`, `body`, `map_iframe`, `address`, `phone`, `mail`, `facebook_link`, `linkedin_link`, `working_hours`, `inner_title`, `forum_flag`) VALUES
+(3, 'Welcome note', '<p>\r\n	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\r\n', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1),
+(4, 'Happening now', '<p>\r\n	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\r\n', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
