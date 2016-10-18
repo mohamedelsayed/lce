8c9ac2f9fa9cf983b2965bbd71934886ada3e4a1
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-10-18 12:51:00 +0200

    shift

--- app/controllers/texts_controller.php
+++ app/controllers/texts_controller.php
@@ -103,4 +103,33 @@ class TextsController extends AppController {
 			$this->autoRender = false;
 		}
 	}       
+	function ics(){
+		$name = '';
+		if(isset($_GET['title'])){
+			$name = $_GET['title'];
+		}
+		$description = '';
+		if(isset($_GET['desc'])){
+			$description = $_GET['desc'];
+		}
+		$start = '';
+		if(isset($_GET['start'])){
+			$start = $_GET['start'];
+		}
+		$end = '';
+		if(isset($_GET['end'])){
+			$end = $_GET['end'];
+		}
+		$location = '';
+		if(isset($_GET['loc'])){
+			$location = $_GET['loc'];
+		}
+		$data = "BEGIN:VCALENDAR\nVERSION:2.0\nMETHOD:PUBLISH\nBEGIN:VEVENT\nDTSTART:".date("Ymd\THis\Z",strtotime($start))."\nDTEND:".date("Ymd\THis\Z",strtotime($end))."\nLOCATION:".$location."\nTRANSP: OPAQUE\nSEQUENCE:0\nUID:\nDTSTAMP:".date("Ymd\THis\Z")."\nSUMMARY:".$name."\nDESCRIPTION:".$description."\nPRIORITY:1\nCLASS:PUBLIC\nBEGIN:VALARM\nTRIGGER:-PT10080M\nACTION:DISPLAY\nDESCRIPTION:Reminder\nEND:VALARM\nEND:VEVENT\nEND:VCALENDAR\n";
+		header("Content-type:text/calendar");
+        header('Content-Disposition: attachment; filename="'.$name.'.ics"');
+        Header('Content-Length: '.strlen($data));
+        Header('Connection: close');
+        echo $data;			
+		$this->autoRender = false;
+	}
 }
