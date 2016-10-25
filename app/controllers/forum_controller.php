58b737a94362ebc09831deb69e3b6303c7bfad77
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-10-25 03:00:32 +0200

    shift

--- app/controllers/forum_controller.php
+++ app/controllers/forum_controller.php
@@ -273,9 +273,15 @@ class ForumController  extends AuthfrontController {
 	function admin_all(){
 		$this->check_isAdmin_isSuperAdmin();
 	}
-	function view_downloadable_file($file_name = ''){
-		$file = BASE_URL.'/app/webroot/files/upload/'.$file_name;
-		$url = 'http://docs.google.com/viewer?url='.urlencode($file).'&embedded=true';
-		$this->redirect($url);
+	function view_downloadable_file(){
+		$file_name = '';
+		if(isset($_GET['file_name'])){
+			$file_name = $_GET['file_name'];
+		}
+		if($file_name != ''){
+			$file = BASE_URL.'/app/webroot/files/upload/'.$file_name;
+			$url = 'http://docs.google.com/viewer?url='.$file.'&embedded=true';
+			$this->redirect($url);
+		}
 	}
 }
